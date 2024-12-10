8fd567e95dbf4149da5eb35d8d0de88d8b0c4185
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2024-12-10 13:37:36 +0530

    updated utils and router

---
+++ utils/index.ts
@@ -0,0 +1,11 @@
+/* user jwt token expiration time*/
+export const expireTimer = {
+  expiresIn: '1h'
+};
+
+/* maintained user roles */
+export const user_roles = {
+  super: "SUPER_USER",
+  admin: "ADMIN_USER",
+  staff: "STAFF_USER",
+}
