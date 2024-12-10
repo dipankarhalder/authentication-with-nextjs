54dde318a54d07898b1b706033c4c583fc9521b0
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2024-12-10 17:34:06 +0530

    added UI and API integrated for signup form

--- router/index.tsx
+++ router/index.tsx
@@ -1,9 +1,16 @@
+/* all frontend public routes */
 export const auth_router = {
   login_page: "/",
   register_page: "/sign-up",
   forgot_page: "/forgot-password",
 }
 
+/* all frontend protected routes */
 export const admin_router = {
   dashboard: "/dashboard",
+}
+
+/* all backend public apis */
+export const auth_endpoint = {
+  api_signup: "/api/signup",
 }
